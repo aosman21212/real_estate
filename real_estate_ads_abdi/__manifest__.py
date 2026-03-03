f22273370770a42d48d62f531ad56b2fcb24d18b
aosman21212 <133777547+aosman21212@users.noreply.github.com>
2026-03-03 23:16:14 +0300

    Update __manifest__.py

--- real_estate_ads_abdi/__manifest__.py
+++ real_estate_ads_abdi/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Real Estate Ads",
-    "author": "abdukarim osman , based on the module real_estate_ads",
+    "author": "LeapAI",
     "version": "19.0.1.1.0",
     "description": "Real Estate module to show available properties",
     "category": "Sales",
